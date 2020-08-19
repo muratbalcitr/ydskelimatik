9f17654a08020b4a331186597d7ec0a9375fb335
murat.balci <murat.balci@hamurlabs.com>
2020-08-19 13:43:08 +0300

    fix

--- app/build/generated/source/buildConfig/debug/com/murat/murat/ydskelimatik/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/murat/murat/ydskelimatik/BuildConfig.java
@@ -5,7 +5,7 @@ package com.murat.murat.ydskelimatik;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.murat.murat.ydskelimatik";
+  public static final String APPLICATION_ID = "com.ahmet.kelimatik";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
